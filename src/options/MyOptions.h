17c884628bde3a938402a64c4b583920bf0f4ee9
Jonas Kunze <kunze.jonas@gmail.com>
2014-05-13 15:12:33 +0200

    Now using Options from na62-farm-lib

---
+++ src/options/MyOptions.h
@@ -0,0 +1,65 @@
+/*
+ * MyOptions.h
+ *
+ *  Created on: Apr 11, 2014
+ \*      Author: Jonas Kunze (kunze.jonas@gmail.com)
+ */
+
+#pragma once
+#ifndef MYOPTIONS_H_
+#define MYOPTIONS_H_
+
+#include <options/Options.h>
+#include <string>
+#include <boost/thread.hpp>
+
+/*
+ * Available options
+ */
+#define OPTION_L0_RECEIVER_PORT (char*)"L0Port"
+#define OPTION_DATA_SOURCE_IDS (char*)"L0DataSourceIDs"
+
+#define OPTION_MEPS_PER_BURST (char*)"NumberOfMEPsPerBurst"
+#define OPTION_RECEIVER_MAC (char*)"ReceiverMAC"
+#define OPTION_RECEIVER_IP (char*)"ReceiverIP"
+
+/*
+ * Performance
+ */
+#define OPTION_ZMQ_IO_THREADS (char*)"zmqIoThreads"
+
+namespace na62 {
+class MyOptions: public Options {
+public:
+	MyOptions();
+	virtual ~MyOptions();
+
+	static void Load(int argc, char* argv[]) {
+		desc.add_options()
+
+		(OPTION_CONFIG_FILE,
+				po::value<std::string>()->default_value(
+						"/etc/na62-telsim.conf"),
+				"Config file for the options shown here")
+
+		(OPTION_L0_RECEIVER_PORT, po::value<int>()->default_value(58913),
+				"UDP-Port for L1 data reception")
+
+		(OPTION_DATA_SOURCE_IDS, po::value<std::string>()->required(),
+				"Comma separated list of all available data source IDs sending Data to L1 (all but LKr) together with the expected numbers of packets per source. The format is like following (A,B,C are sourceIDs and a,b,c are the number of expected packets per source):\n \t A:a,B:b,C:c")
+
+		(OPTION_MEPS_PER_BURST, po::value<int>()->required(),
+				"Number of MEPs to be sent per burst")
+
+		(OPTION_RECEIVER_MAC, po::value<std::string>()->required(),
+				"MAC address where the MEPs should be sent to")
+
+		(OPTION_RECEIVER_IP, po::value<std::string>()->required(),
+				"IP address where the MEPs should be sent to");
+
+		Options::Initialize(argc, argv, desc);
+	}
+};
+} /* namespace na62 */
+
+#endif /* MYOPTIONS_H_ */
